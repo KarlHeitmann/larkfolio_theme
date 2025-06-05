d6f1df6db0560c93e6a1797fdc0cf74830489ff7
Karl Heitmann <khf1988@gmail.com>
2025-06-05 00:17:11 -0400

    bugfix: ajax template experiences-results.php had bug

--- themes/larkfolio/template-parts/ajax/experiences-results.php
+++ themes/larkfolio/template-parts/ajax/experiences-results.php
@@ -12,11 +12,16 @@ foreach ($experiences as $experience) {
   $post = get_post($experience); // Ensure it's a full WP_Post object
   setup_postdata($post);
   get_template_part('template-parts/card-experience', null, array(
-    'permalink' => get_permalink($post),
-    'title' => get_the_title($post),
-    'excerpt' => get_the_excerpt($post),
-    'repository_link' => get_field('repository_link', $post->ID),
-    'pr_link' => get_field('pr_link', $post->ID)
+    'company_website' => get_field('company_website'),
+    'related_skills' => get_field('related_skills'),
+    'permalink' => get_the_permalink(),
+    'excerpt' => get_the_excerpt(),
+    'title' => get_the_title(),
+    'job_title' => get_field('job_title'),
+    'start_date' => get_field('start_date'),
+    'end_date' => get_field('end_date'),
+    'location' => get_field('location'),
+    'remote' => get_field('remote'),
   ));
 }
 wp_reset_postdata();
