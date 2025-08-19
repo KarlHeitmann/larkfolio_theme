c7b9a9629a2f926f4460be58a3f1f771e285ca05
Karl Heitmann <khf1988@gmail.com>
2025-08-19 18:15:02 -0400

    displays nested pages in all cases

--- themes/larkfolio/page.php
+++ themes/larkfolio/page.php
@@ -3,15 +3,20 @@ get_header();
 ?>
 
 <div class="container mx-auto px-4">
-  <?php
-  while(have_posts()) {
-    the_post();
-    ?>
-    <!-- <h1><?php the_title(); ?></h1> -->
-    <?php the_content(); ?>
-    <?php
-  }
-  ?>
+  <section class="grid grid-cols-4">
+    <article class="col-span-3">
+      <?php get_template_part('page/main_content'); ?>
+    </article>
+    <aside>
+      <?php
+      wp_list_pages(array(
+        'title_li' => null, // Removes "Pages" title
+        'child_of' => get_the_ID(),
+        'sort_column' => 'menu_order' // By default, wp_list_pages will order pages by name. This changes it to order by menu order. This order value can be tweaked by page in "Order" input box in WP dashboard admin of the page, in its "Page Attributes" section
+      ));
+      ?>
+    </aside>
+  </section>
 </div>
 <?php
 get_footer();
