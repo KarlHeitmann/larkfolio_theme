bb51fec7073fed74172062aa6dd511f1f4038050
Karl Heitmann <khf1988@gmail.com>
2025-08-19 21:56:03 -0400

    breadcrumbs stylized

--- themes/larkfolio/template-parts/widgets/breadcrumbs.php
+++ themes/larkfolio/template-parts/widgets/breadcrumbs.php
@@ -6,7 +6,7 @@ while ($theParent) {
 }
 $ancestors = array_reverse($ancestors);
 ?>
-<div class="flex">
+<div class="flex border border-gray-500 border-2 rounded-lg p-2 my-2">
 <?php foreach ($ancestors as $ancestor) { ?>
   <a
     class="breadcrumb-link"
