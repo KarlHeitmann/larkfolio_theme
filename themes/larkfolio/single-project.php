dcf0f0eb3d3181c34c69a7c52459e4eee0b5ecf9
Karl Heitmann <khf1988@gmail.com>
2025-08-21 23:18:26 -0400

    adds the-content-wrapper to single-project page

--- themes/larkfolio/single-project.php
+++ themes/larkfolio/single-project.php
@@ -18,7 +18,9 @@ get_header();
   while(have_posts()) {
     the_post(); ?>
     <h2 class="text-2xl font-bold my-12"><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+    <section class="the-content-wrapper">
+      <?php the_content(); ?>
+    </section>
     <?php
   }
   ?>
