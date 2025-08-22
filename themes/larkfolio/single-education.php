28d851ba054ea7d18586d5c9a9b8ffed33c62ad0
Karl Heitmann <khf1988@gmail.com>
2025-08-21 23:38:00 -0400

    fixed single-project page, and removed title from education instead

--- themes/larkfolio/single-education.php
+++ themes/larkfolio/single-education.php
@@ -12,8 +12,12 @@ get_header();
   <?php
   while(have_posts()) {
     the_post(); ?>
+    <!--
     <h2 class="text-2xl font-bold my-12"><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+  -->
+    <section class="the-content-wrapper">
+      <?php the_content(); ?>
+    </section>
     <?php
   }
   ?>
